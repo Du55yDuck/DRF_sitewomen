43f33ebe32e355e3a9991a70c586f6f6a51c28fe
Denis Riabov <zwx7000@mail.ru>
2024-05-29 21:52:47 +0400

    Add simple jwt. Add auth JWT-tokens

--- drfsitewomen/drfsitewomen/settings.py
+++ drfsitewomen/drfsitewomen/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,10 +138,50 @@ REST_FRAMEWORK = {  # Управление глобальными настрой
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [  # разрешения для ДРФ использовать auth
-        'rest_framework.authentication.TokenAuthentication',  # по токенам
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # по JWT-токенам
         'rest_framework.authentication.BasicAuthentication',  # базовая
         'rest_framework.authentication.SessionAuthentication',  # по сессиям
     ]
 }
 
+
+SIMPLE_JWT = {  # настройки для simple jwt
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),  # жизнь accross-токена
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),  # жизнь refresh-токена
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",  # метод шифрования
+    "SIGNING_KEY": SECRET_KEY,  # секретный ключ Джанго
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),  # заголовок перед токеном в URL
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",  # идентификатор с pk пользователя
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",  # жизнь "протухания" токенов в cookies
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
 
